8f69cdc6ce08d14274bf0f3ac3ac569d6018748e
Alireza Mehrtash <mehrtash@bwh.harvard.edu>
2013-08-27 20:26:37 -0400

    DCMTK example-1 added

---
+++ DCMTK/Example-1/testapp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "dcmtk/config/osconfig.h"
+#include "dcmtk/dcmdata/dctk.h"
+
+using namespace std;
+
+
+int main()
+{
+	
+	DcmFileFormat fileformat;
+
+OFCondition status = fileformat.loadFile("test.dcm");
+if (status.good())
+{
+   OFString patientsName;
+   if (fileformat.getDataset()->findAndGetOFString(DCM_PatientName, patientsName).good())
+   {
+      cout << "Patient's Name: " << patientsName << endl;
+   } else
+     cerr << "Error: cannot access Patient's Name!" << endl;
+} else
+cerr << "Error: cannot read DICOM file (" << status.text() << ")" << endl;
+
+return 0;
+}
